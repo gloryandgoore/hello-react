6756b9c0c560307deb15429e9f1e3f955c6e0fbd
Caseyann Goore <93567950+gloryandgoore@users.noreply.github.com>
2022-01-18 01:26:17 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const Button = (props) => {
-  
-  return (
-    <button onClick={props.onClick}>{props.name}</button>
-
-
-  );
+  return <button onClick={props.onClick}>{props.name}</button>;
 };
 
 const Application = () => {
- const [name, setName] = useState(" ");
+  const formText = "Type your name";
+  const [name, setName] = useState("");
 
   // your code here
 
   const reset = () => {
     console.log("reset");
+    onClick = setName("");
     // your code here
   };
 
   return (
-   
-
     <main>
-  <input value={name} onChange={(event) => setName(event.target.value)} placeholder="Type your name"></input>
-    
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Type your name"
+      ></input>
+
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <Button name="Reset" onClick={reset}>
-      
-      </Button>
-      <h1>Hello {name}</h1>
-      
+      <Button name="Reset" onClick={reset}></Button>
+
+      {name && <h1>Hello {name}</h1>}
+      {!name && <h1></h1>}
     </main>
   );
 };
